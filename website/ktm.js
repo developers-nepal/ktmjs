c7451649f2742f4ca703705dec987a965dcd0b1b
ghome vic <green.rabi@hotmail.com>
2015-09-20 19:48:51 +0530

    Express loading Admin page.

--- website/ktm.js
+++ website/ktm.js
@@ -1,15 +1,20 @@
 'use strict';
 var express = require('express');
+var open = require('open');
 
 console.log('Namaste.\nWelcome to Kathmandu Javascript community.');
 
 var server = express();
 var port = process.env.PORT || 3000;
 
+server.use(express.static('admin'));
+
 server.listen(port, function() {
   console.log('server is up and running on', port);
-});
-
-server.use('/', function(req, res) {
-  res.send('Hello World');
+  console.log('Opening Admin.');
+  open("http://localhost:" + port, function(err) {
+    if (err) {
+      console.log('Unable to open browser, try http://localhost:3000 instead.', err);
+    }
+  });
 });
