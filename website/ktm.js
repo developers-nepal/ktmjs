4d52053dbe7cd8e4ee790d8875339537c03d955a
ghome vic <green.rabi@hotmail.com>
2015-09-20 20:35:58 +0530

    Save Event information.

--- website/ktm.js
+++ website/ktm.js
@@ -1,6 +1,11 @@
 'use strict';
+var bodyParser = require('body-parser');
 var express = require('express');
 var open = require('open');
+var fs = require('fs');
+var path = require('path');
+
+var episodes = require('./db/Meetup.json');
 
 console.log('Namaste.\nWelcome to Kathmandu Javascript community.');
 
@@ -8,6 +13,33 @@ var server = express();
 var port = process.env.PORT || 3000;
 
 server.use(express.static('admin'));
+server.use(bodyParser.json());       // to support JSON-encoded bodies
+server.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+/* routes */
+server.post('/saveevent', function(req, res) {
+  console.log('params', req.body);
+  var params = req.body;
+
+  if (params.date && params.venue) {
+    episodes.push({
+      "date": params.date,
+      "venue": params.venue
+    });
+
+    fs.writeFileSync(
+      path.join(__dirname, 'db/Meetup.json'),
+      JSON.stringify(episodes),
+      'utf8',
+      function(err) {
+        if (err) throw err;
+      });
+
+    res.send('Got it!');
+  }
+});
 
 server.listen(port, function() {
   console.log('server is up and running on', port);
