049a247b6e288f2f0c8ef4a47fc7c71240829e18
Pratik <pratik.manandhar99@gmail.com>
2018-01-11 12:41:32 +0545

    added db persistence and remvoed image file

--- site-admin/routes/people.js
+++ site-admin/routes/people.js
@@ -3,19 +3,37 @@
 var express = require('express'),
   router = express.Router();
 
+var path = require('path');
 var db = require('../db.js')();
+var fs = require('fs');
 
-router.get('/', function(req, res) {
-  db.people.find({}, function(err, docs) {
+router.get('/', function (req, res) {
+  db.people.find({}, function (err, docs) {
     res.render('admin/people_list', { "people": docs });
   });
 });
 
 router.get('/delete/:id', function (req, res) {
   let personId = req.params.id;
-  db.people.remove({ "_id": personId }, {}, function (error) {
-    res.redirect('/people');
+  db.people.findOne({ "_id": personId }, {}, function (error, doc) {
+
+    if (!error) {
+      db.people.remove({ "_id": personId }, {}, function (error, numRemoved) {
+        if (!error) {
+          db.people.persistence.compactDatafile();
+          fs.exists(doc.file.path, function (exists) {
+            if (exists) {
+              fs.unlink(doc.file.path, function (error) {
+                let feedback = error ? error : 'image deleted as well';
+                console.log(feedback);
+              })
+            }
+          })
+        }
+      });
+    }
   })
+  res.redirect('/people');
 });
 
 router.get('/form', function (req, res) {
